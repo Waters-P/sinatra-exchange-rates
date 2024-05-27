1801fa5a800c4d9ee7de2c7eacb21ff198389289
Waters <166139510+Waters-P@users.noreply.github.com>
2024-05-27 14:51:09 +0000

    fetch json

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
+
+
+
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  
+
+  exchange_rate_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+# Place a GET request to the URL
+  @raw_res = HTTP.get(exchange_rate_url)
+
+  @raw_res = @raw_res.to_s
+
+  @parsed_data = JSON.parse(@raw_res)
+
+  erb(:root)
+
+
+
 end
+
+
+
+pp ENV.fetch("EXCHANGE_RATE_KEY")
